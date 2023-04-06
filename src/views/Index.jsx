5aeda78b69a3a5a5186e7b5cb2732834b2fd4ff7
Oscar Varela <oscarvarela@MacBook-Air-de-Oscar.local>
2023-04-06 03:00:59 -0600

    index

---
+++ src/views/Index.jsx
@@ -0,0 +1,239 @@
+import React from 'react'
+import img from '../assets/img/index.jpg'
+import { Row, Col, Card } from 'react-bootstrap'
+
+
+const sectionStyle = {
+    backgroundImage: `url(${img})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh',
+}
+
+const Index = () => {
+
+
+
+    return (
+        <>
+
+            <Row>
+                <h1
+                    style={{
+                        textAlign: 'center',
+                        color: 'white',
+                        fontSize: '3rem',
+                        position: 'absolute',
+                        top: '20%',
+                        left: '-35%',
+                    }}
+
+                >Tienda de Ropa</h1>
+                <Col
+                    style={{ backgroundColor: '#003366', height: '80vh' }}
+                >
+
+                </Col>
+                <div
+                    style={{
+                        backgroundColor: 'white',
+                        width: '50%',
+                        borderRadius: '10px',
+                        position: 'absolute',
+                        top: '45%',
+                        left: '25%',
+                    }}
+                >
+                    <h1
+                        style={{ textAlign: 'center', color: 'black', fontSize: '3rem' }}
+                    >   50%
+                        Descuento
+                        <br />
+                        en tu primera compra
+                        <br />
+                        ¡Registrate
+                        para obtener tu cupón!
+                    </h1>
+                </div>
+                <Col
+                    style={{ backgroundColor: '#884507', height: '80vh' }}
+                >
+
+                </Col>
+            </Row>
+            <Row>
+                <Col >
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+
+                    </Card>
+
+                </Col>
+                <Col >
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+
+                    </Card>
+
+                </Col>
+                <Col >
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+
+                    </Card>
+
+                </Col>
+                <Col >
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+
+                    </Card>
+
+                </Col>
+                <Col >
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+
+                    </Card>
+
+                </Col>
+                <Row>
+                <Col >
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+
+                    </Card>
+
+                </Col>
+                <Col >
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+
+                    </Card>
+
+                </Col>
+                </Row>
+                    <Row>
+
+                    <Col 
+                    style={{ backgroundColor: '#003366', height: '80vh',borderRadius:'10px' }}
+
+                    >
+                        <Card
+                                style={{ width: '100%', height: '100%',opacity: '0.8'   }}                   
+                            >
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted">Last updated 3 mins ago</small>
+                            </Card.Footer>
+                                </Card>
+                        </Col>
+                        <Col 
+                    style={{ backgroundColor: 'white', height: '80vh' }}
+                    >
+                        <h2
+                        style={{ color: 'black', fontSize: '3rem',
+                        fontFamily: 'sans-serif',
+                        fontWeight: 'bold',
+                        position: 'absolute',
+                        top: '',
+                        left: '',
+                        bottom: '-100%',
+                        right: '1%',}}
+                        >
+                            Las mejores ofertas en ropa 
+                            <br />
+                            <span style={{color:'#003366'}}>para toda la familia</span>
+                            <br />
+                            <span style={{color:'red'}}>¡Registrate para obtener tu cupón!</span>
+                        </h2>
+                        </Col>
+                    </Row>
+
+                
+            </Row>
+           
+
+        </>
+
+    )
+}
+
+export default Index
--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {  Row, Col, Form, Button, Alert } from 'react-bootstrap'
+import { Row, Col, Form, Button, Alert } from 'react-bootstrap'
 import { Link, useNavigate } from 'react-router-dom'
 
 const Register = () => {
@@ -12,7 +12,7 @@ const Register = () => {
 
     const handleRegister = (e) => {
         e.preventDefault()
-        const userRegister ={
+        const userRegister = {
             name: e.target[0].value,
             lastname: e.target[1].value,
             email: e.target[2].value,
@@ -27,15 +27,15 @@ const Register = () => {
             setValidated(true)
             return
         }
-       
+
         if (userRegister.password !== userRegister.password2) {
             setMessage('Las contraseñas no coinciden')
             setValidated(true)
             return
-        } 
+        }
         setValidated(false)
         setMessage('')
-           
+
         const sendUserRegister = async (userRegister) => {
             try {
                 const response = await fetch('http://localhost:4000/api/register', {
@@ -47,11 +47,11 @@ const Register = () => {
                 })
                 const data = await response.json()
                 console.log(data)
-                
+
             } catch (error) {
-                    console.log(error)
+                console.log(error)
             }
-        
+
         }
         sendUserRegister(userRegister)
 
@@ -60,125 +60,125 @@ const Register = () => {
         <Row>
 
             <Col md={{ span: 8, offset: 2 }}>
-            <Form.Group className='mb-3'>
-                                    <Form.Label>
-                                            <Button variant='primary' onClick={handleReturn}>Volver</Button>
-                                    </Form.Label>
-                                </Form.Group>
+                <Form.Group className='mb-3'>
+                    <Form.Label>
+                        <Button variant='primary' onClick={handleReturn}>Volver</Button>
+                    </Form.Label>
+                </Form.Group>
                 <h2>Registrate</h2>
                 {
                     validated && <Alert variant='danger'>{message}</Alert>
                 }
-              <Col>
-                               
-                            </Col>
-                    <Form
-                        onSubmit={handleRegister}>
-                        <Row>
-                            <Col>
+                <Col>
 
-                                <Form.Group className='mb-3'>
-                                    <Form.Label>
-                                        Ingresa tu nombre
-                                    </Form.Label>
-                                    <Form.Control type='text' placeholder='Ingresa tu nombre '>
-                                    </Form.Control>
-                                </Form.Group>
-                            </Col>
-                            <Col>
+                </Col>
+                <Form
+                    onSubmit={handleRegister}>
+                    <Row>
+                        <Col>
 
-                                <Form.Group className='mb-3'>
-                                    <Form.Label>
-                                       Apellidos
-                                    </Form.Label>
-                                    <Form.Control type='text' placeholder='Apellidos '>
-                                    </Form.Control>
-                                </Form.Group>
-                            </Col>
+                            <Form.Group className='mb-3'>
+                                <Form.Label>
+                                    Nombre
+                                </Form.Label>
+                                <Form.Control type='text' placeholder='Ingresa tu nombre '>
+                                </Form.Control>
+                            </Form.Group>
+                        </Col>
+                        <Col>
 
-                        </Row>
-                        <Row>
-                            <Col>
-                                <Form.Group className='mb-3'>
-                                    <Form.Label>
-                                        Ingresa tu correo electronico
-                                    </Form.Label>
-                                    <Form.Control type='email' placeholder='Ingresa tu correo electronico '>
-                                    </Form.Control>
+                            <Form.Group className='mb-3'>
+                                <Form.Label>
+                                    Apellidos
+                                </Form.Label>
+                                <Form.Control type='text' placeholder='Apellidos '>
+                                </Form.Control>
+                            </Form.Group>
+                        </Col>
 
-                                </Form.Group>
-                            </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Form.Group className='mb-3'>
+                                <Form.Label>
+                                    Ingresa tu correo electronico
+                                </Form.Label>
+                                <Form.Control type='email' placeholder='Ingresa tu correo electronico '>
+                                </Form.Control>
 
-                        </Row>
-                        <Row>
-                            <Col>
-                                <Form.Group className='mb-3'>
-                                    <Form.Label>
-                                        Ingresa tu contraseña
-                                    </Form.Label>
-                                    <Form.Control type='password' placeholder='Ingresa tu contraseña '>
-                                    </Form.Control>
+                            </Form.Group>
+                        </Col>
 
-                                </Form.Group>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <Form.Group className='mb-3'>
-                                    <Form.Label>
-                                        Repite tu contraseña
-                                    </Form.Label>
-                                    <Form.Control type='password' placeholder='Repite tu contraseña '>
-                                    </Form.Control>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Form.Group className='mb-3'>
+                                <Form.Label>
+                                    Ingresa tu contraseña
+                                </Form.Label>
+                                <Form.Control type='password' placeholder='Ingresa tu contraseña '>
+                                </Form.Control>
 
-                                </Form.Group>
-                            </Col>
-                        </Row>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Form.Group className='mb-3'>
+                                <Form.Label>
+                                    Repite tu contraseña
+                                </Form.Label>
+                                <Form.Control type='password' placeholder='Repite tu contraseña '>
+                                </Form.Control>
 
-                        <Row>
-                            <Col>
-                                <Form.Group className='mb-3'>
-                                    <Form.Label>
-                                        Ingresa tu numero de telefono
-                                    </Form.Label>
-                                    <Form.Control type='text' placeholder='Ingresa tu numero de telefono '>
-                                    </Form.Control>
+                            </Form.Group>
+                        </Col>
+                    </Row>
 
-                                </Form.Group>
-                                </Col>
-                               
-                                <Col>
-                                <Form.Group className='mb-3'>
-                                    <Form.Label>
-                                        Fecha de nacimiento
-                                    </Form.Label>
-                                    <Form.Control type='date' placeholder='Ingresa tu nombre '>
-                                    </Form.Control>
+                    <Row>
+                        <Col>
+                            <Form.Group className='mb-3'>
+                                <Form.Label>
+                                    Ingresa tu numero de telefono
+                                </Form.Label>
+                                <Form.Control type='text' placeholder='Ingresa tu numero de telefono '>
+                                </Form.Control>
 
-                                </Form.Group>
-                             </Col>
+                            </Form.Group>
+                        </Col>
 
-                               
-                        
-                           
-                        </Row>
-                      
-                        <Row>
-                           
-                            <Col>
-                                <Form.Group className='mb-3'>
-                                    <Form.Label>
-                                        <Button variant='success'   
+                        <Col>
+                            <Form.Group className='mb-3'>
+                                <Form.Label>
+                                    Fecha de nacimiento
+                                </Form.Label>
+                                <Form.Control type='date' placeholder='Ingresa tu nombre '>
+                                </Form.Control>
+
+                            </Form.Group>
+                        </Col>
+
+
+
+
+                    </Row>
+
+                    <Row>
+
+                        <Col>
+                            <Form.Group className='mb-3'>
+                                <Form.Label>
+                                    <Button variant='success'
                                         type='submit'>Registrarse</Button>
-                                    </Form.Label>
-                                </Form.Group>
-                            </Col>
-                        </Row>
+                                </Form.Label>
+                            </Form.Group>
+                        </Col>
+                    </Row>
 
-                    </Form>
+                </Form>
 
 
-            
+
             </Col>
 
         </Row>
